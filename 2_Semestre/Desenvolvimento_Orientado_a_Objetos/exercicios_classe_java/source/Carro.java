48937b555dd85e569a27fc19c91796d79d50bfbf
felipeteixeiradelima <felipe.teixeiralima2511@gmail.com>
2024-10-08 18:42:42 -0300

    Update em Desenvolvimento Orientado a Objetos
    O arquivo "Carro.java" da pasta "exercicios_classe_java" foi modificado. Foram adicionados comentários explicando o código.

--- 2_Semestre/Desenvolvimento_Orientado_a_Objetos/exercicios_classe_java/source/Carro.java
+++ 2_Semestre/Desenvolvimento_Orientado_a_Objetos/exercicios_classe_java/source/Carro.java
@@ -1,9 +1,12 @@
+//classe Carro
 public class Carro {
+    //atributos da classe Carro
     int ano;
     String marca;
     String modelo;
     String cor;
 
+    //constructor da classe Carro (associa os parâmetros aos atributos da instância)
     Carro(String marca, String modelo, String cor, int ano) {
         this.marca = marca;
         this.modelo = modelo;
@@ -11,9 +14,12 @@ public class Carro {
         this.ano = ano;
     }
 
+    //método da classe Carro que exibe o modelo da instância
     public void exibirModelo() {
         System.out.printf("O modelo desse carro é %s %s\n",marca, modelo);
     }
+
+    //método da classe Carro que exibe o ano e a cor da instância
     public void exibirAnoECor() {
         System.out.printf("Esse carro foi fabricado em %d e sua cor é %s\n",ano, cor);
     }
