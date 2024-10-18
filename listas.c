ee934779567b2644a58133b21505f31e22d2cbe7
felipeteixeiradelima <felipe.teixeiralima2511@gmail.com>
2024-10-18 07:54:41 -0300

    Update listas.c
    Programa se encontra funcional; ainda em fase de testes.

--- listas.c
+++ listas.c
@@ -27,15 +27,11 @@ int isPilhaCheia() {
     unsigned int contador = 0;
     struct listasimples *aux;
     aux = pilha;
-    
-    while (aux->prox != NULL){
+    do {
         contador++;
         aux = aux->prox;
-    }
-    
-    aux = NULL;
-    free(aux);
-    
+    } while (aux);
+
     if (contador == TAM_MAX) return 1;
     return 0;
 }
@@ -43,89 +39,106 @@ int isPilhaCheia() {
 //função de inserir na pilha
 int inserirPilha() {
 
-    //se a pilha estiver cheia (TAM_MAX) retorna 0
-    if (isPilhaCheia(pilha)) return 0;
+    //se a pilha estiver cheia (TAM_MAX) retorna 1 indicando erro
+    if (isPilhaCheia()) return 1;
     
-    //criação de um ponteiro auxiliar
-    struct listasimples *aux;
-    aux = (struct listasimples*) malloc(sizeof(struct listasimples));
+    //se a pilha estiver vazia
+    else if (!topoPilha) {
+        scanf("%d", &pilha->valor);
+        topoPilha = pilha;
+    }
     
-    scanf("%d", &aux->valor);
-    aux->prox = NULL;
+    //se a pilha tiver pelo menos 1 elemento
+    else {
+        //criação de um ponteiro auxiliar
+        struct listasimples *aux;
+        aux = (struct listasimples*) malloc(sizeof(struct listasimples));
+
+        scanf("%d", &aux->valor);
+        aux->prox = NULL;
     
-    //topo->prox aponta para o novo topo
-    topoPilha->prox = aux;
+        //topo->prox aponta para o novo topo
+        topoPilha->prox = aux;
+
+        //topo recebe aux (novo topo)
+        topoPilha = aux;
+        
+        if (aux) printf("\naux - inserir: %i %p %p\n", aux->valor, aux->prox, aux);
+    }
     
-    //topo recebe aux (novo topo)
-    topoPilha = aux;
-    
-    //limpa o ponteiro aux
-    //aux = NULL;
-    //free(aux);
-    
-    printf("\naux - inserir: %i %p %p\n", aux->valor, aux->prox, aux);
     printf("\ntopoPilha - inserir: %i %p %p\n", topoPilha->valor, topoPilha->prox, topoPilha);
-    //retorna 1 indicando sucesso
-    return 1;
+    //retorna 0 indicando sucesso
+    return 0;
 }
 
 //função de remover da pilha
 int removerPilha() {
-    //se a pilha estiver vazia retorna 0
-    if (pilha->prox == NULL) return 0;
+    //se a pilha estiver vazia retorna 1 indicando erro
+    if (!topoPilha) return 1;
     
-    struct listasimples *aux, *aux2;
-    aux = pilha;
+    //caso haja apenas 1 elemento na pilha:
+    else if (topoPilha == pilha) {
+        //aponta o topo para NULL
+        topoPilha = NULL;
+    }
     
-    //achar o penultimo item da pilha (1 antes do topo) e atribuir a aux
-    while (aux->prox != topoPilha) aux = aux->prox;
-    
-    //trocar o topo com o aux
-    aux2 = aux;
-    aux = topoPilha;
-    topoPilha = aux2;
-    
-    //remover o proximo do topo
-    topoPilha->prox = NULL;
-    
-    //limpar os ponteiros
-    aux2 = NULL;
-    free(aux);
-    free(aux2);
-    
-    //retorna 1 indicando sucesso
-    return 1;
+    //se houver >1 elementos na pilha:
+    else {
+        //cria 1 ponteiro auxiliar
+        struct listasimples *aux;
+        aux = pilha;
+
+        //achar o penultimo item da pilha (1 antes do topo) e atribuir a aux
+        while (aux->prox != topoPilha) aux = aux->prox;
+
+        //removendo o topo
+        free(topoPilha);
+        
+        //removendo o proximo do aux (novo topo)
+        aux->prox = NULL;
+        
+        //topo recebe aux (novo topo)
+        topoPilha = aux;
+    }
+
+    //retorna 0 indicando sucesso
+    return 0;
 }
 
 //função para exibir a pilha
 int exibirPilha() {
-    //se a pilha estiver vazia retorna 0 indicando erro
-    if (pilha->prox == NULL) return 0;
+    //se a pilha estiver vazia retorna 1 indicando erro
+    if (!topoPilha) return 1;
     
-    //criação de um ponteiro auxiliar
-    struct listasimples *aux;
-    
-    //aux recebe o início da pilha
-    aux = pilha;
-    
-    //exibe todos os elementos da pilha (valor e endereço)
-    while (aux->prox != NULL) {
-        printf("%i %p", aux->valor, aux);
-        aux = aux->prox;
+    //caso haja apenas 1 elemento na pilha
+    else if (topoPilha == pilha) {
+        printf("%i %p\n", pilha->valor, pilha);
     }
     
-    //limpa o ponteiro aux
-    aux = NULL;
-    free(aux);
-    //retorna 1 indicando sucesso
-    return 1;
+    //se houver >1 elementos na pilha
+    else {
+        //criação de um ponteiro auxiliar
+        struct listasimples *aux;
+
+        //aux recebe o início da pilha
+        aux = pilha;
+
+        //exibe todos os elementos da pilha (valor e endereço)
+        do {
+            printf("%i %p\n", aux->valor, aux);
+            aux = aux->prox;
+        } while (aux);
+    }
+    
+    //retorna 0 indicando sucesso
+    return 0;
 }
 
 unsigned short getOption() {
     unsigned short o;
     do {
         printf("Escolha uma opção: ");
-        o =0;
+        o = 0;
         if (!scanf("%hu", &o)) getchar();
     } while (o < 1 && o > 4);
     return o;
@@ -135,10 +148,10 @@ int main() {
     setlocale(LC_ALL, "Portuguese");
     pilha = (struct listasimples*) malloc(sizeof(struct listasimples));
     pilha->prox = NULL;
-    topoPilha = pilha;
-    
+    topoPilha = NULL;
+
     unsigned short opcao;
-    
+
     do {
         puts("Opções:\n");
         puts("1. Inserir");
@@ -148,16 +161,15 @@ int main() {
         opcao = getOption();
         switch (opcao) {
             case 1:
-                if (!inserirPilha(topoPilha)) puts("A pilha está cheia!\n");
+                if (inserirPilha(topoPilha)) puts("A pilha está cheia!\n");
                 break;
             case 2:
-                if (!removerPilha(pilha, topoPilha)) puts("A pilha está vazia!\n");
+                if (removerPilha(pilha, topoPilha)) puts("A pilha está vazia!\n");
                 break;
             case 3:
-                if (!exibirPilha(pilha)) puts("A pilha está vazia!\n");
+                if (exibirPilha(pilha)) puts("A pilha está vazia!\n");
                 break;
         }
-        printf("\ntopoPilha - main: %i %p %p\n\n", topoPilha->valor, topoPilha->prox, topoPilha);
+        if (topoPilha) printf("\ntopoPilha - main: %i %p %p\n\n", topoPilha->valor, topoPilha->prox, topoPilha);
     } while (opcao != 4);
-    
 }
