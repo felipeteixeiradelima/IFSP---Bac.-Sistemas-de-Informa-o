1ab118ba214952261d11db2d42ad7eb06c6b07eb
felipeteixeiradelima <felipe.teixeiralima2511@gmail.com>
2024-10-23 18:22:46 -0300

    Update em Desenvolvimento Orientado a Objetos
    Foram adicionados comentários no arquivo "Pessoa.java" em "exercicio_classe_java_2".

--- 2_Semestre/Desenvolvimento_Orientado_a_Objetos/exercicio_classe_java_2/source/Pessoa.java
+++ 2_Semestre/Desenvolvimento_Orientado_a_Objetos/exercicio_classe_java_2/source/Pessoa.java
@@ -1,14 +1,19 @@
+//classe Pessoa
 public class Pessoa {
+
+    //atriburos de Pessoa
     String nome;
     int idade;
     Cao pet;
 
+    //construtor de Pessoa
     Pessoa (String nome, int idade, Cao pet) {
         this.nome = nome;
         this.idade = idade;
         this.pet = pet;
     }
     
+    //método pedirPraCaoLatir(): imprime que [nome da pessoa] está pedindo para [nome do cão] latir e invoca o método latir() do pet
     void pedirPraCaoLatir() {
         System.out.printf("%s está pedindo para %s latir!\n", nome, pet.nome);
         pet.latir();
