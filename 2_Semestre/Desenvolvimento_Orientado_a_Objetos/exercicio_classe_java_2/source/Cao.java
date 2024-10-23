8d3ee69e64d6fa06d10aef7514951cadc03694f4
felipeteixeiradelima <felipe.teixeiralima2511@gmail.com>
2024-10-23 18:23:26 -0300

    Update em Desenvolvimento Orientado a Objetos
    Foram adicionados comentários no arquivo fonte "Cao.java" em "exercicio_classe_java_2".

--- 2_Semestre/Desenvolvimento_Orientado_a_Objetos/exercicio_classe_java_2/source/Cao.java
+++ 2_Semestre/Desenvolvimento_Orientado_a_Objetos/exercicio_classe_java_2/source/Cao.java
@@ -1,12 +1,17 @@
+//classe Cao
 public class Cao {
+
+    //atributos de Cao
     String nome, raca, somLatido;
 
+    //construtor de Cao
     Cao (String nome, String raca, String somLatido) {
         this.nome = nome;
         this.raca = raca;
         this.somLatido = somLatido;
     }
 
+    //metodo latir(): imprime que [nome do cão] está latindo e o som do latido [somLatido]
     void latir() {
         System.out.printf("%s está latindo: %s\n", nome, somLatido);
     }
