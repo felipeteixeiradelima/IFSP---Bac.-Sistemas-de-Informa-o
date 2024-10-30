c2827eda462b74ae11866f20f468fc23fdc67757
felipeteixeiradelima <felipe.teixeiralima2511@gmail.com>
2024-10-30 11:57:20 -0300

    Update em Sistemas Operacionais
    O arquivo chamada-linux.c foi atualizado.

--- 2_Semestre/Sistemas_Operacionais/chamada_de_sistema_linux/source/chamada-linux.c
+++ 2_Semestre/Sistemas_Operacionais/chamada_de_sistema_linux/source/chamada-linux.c
@@ -1,1 +1,23 @@
+#include <sys/stat.h>
+#include <stdio.h>
+#include <string.h>
 
+int main () {
+    char caminho_original[200], caminho_novo[200];
+
+    printf("Digite o caminho original do arquivo (+ seu nome): ");
+    fgets(caminho_original, 200, stdin);
+    printf("Digite o novo caminho do arquivo (+ seu nome): ");
+    fgets(caminho_novo, 200, stdin);
+
+    caminho_original[strlen(caminho_original)-1] = 0;
+    caminho_novo[strlen(caminho_novo)-1] = 0;
+
+    if (rename(caminho_original, caminho_novo) != 0) {
+        puts("Erro! Não foi possível mover o arquivo!");
+        return 1;
+    }
+
+    puts("Arquivos movidos com sucesso!");
+    return 0;
+}
